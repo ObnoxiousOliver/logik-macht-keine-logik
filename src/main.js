1167cdcd79914fe530173fc89770d6b03984e981
ObnoxiousOliver <maxhd2005@gmail.com>
2021-11-30 15:45:33 +0100

    vuex added

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import VTooltip from 'v-tooltip'
 import Vue3TouchEvents from 'vue3-touch-events'
+import store from './store'
 
-createApp(App)
+createApp(App).use(store)
   .use(VTooltip)
   .use(Vue3TouchEvents)
   .mount('#app')
